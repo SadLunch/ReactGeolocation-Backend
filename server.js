ca28a332d950472a977c1f5d81ce7b5544005c78
SadLunch <olaadeus12345z@gmail.com>
2024-10-17 12:29:39 +0100

    trying with experience locations and seeing if the number of current users in it work

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ const io = new Server(server, {
   }
 });
 
+// 38.65109183413688, -9.173760194424412
+
+let experiences = [
+  {id: 1, location: {lat: 38.710712892523695, lng: -9.14116628132217}, minDistance: 10, name: 'Bertrand', image: '', nUsersIn: 0},
+  {id: 2, location: {lat: 38.709899442837504, lng: -9.141786418367884}, minDistance: 10, name: 'Modelo', image: '', nUsersIn: 0},
+  {id: 3, location: {lat: 38.71051049617013, lng: -9.142228602930569}, minDistance: 10, name: 'Estatua', image: '', nUsersIn: 0},
+  {id: 4, location: {lat: 38.709363862713104, lng: -9.141031676168348}, minDistance: 10, name: 'MUP', image: '', nUsersIn: 0},
+  {id: 5, location: {lat: 38.65109183413688, lng: -9.173760194424412}, minDistance: 20, name: 'Casa Teste', image: '', nUsersIn: 0},
+]
+
 io.on('connection', (socket) => {
   console.log('A user connected');
 
@@ -25,18 +35,23 @@ io.on('connection', (socket) => {
   })
 
   // Listen for location updates from clients
-  socket.on('send-location', (user_location, target_location, user) => {
+  socket.on('send-location', (user_location, user, currentExperience) => {
     console.log('User location:', user_location);
-    console.log('Target location:', target_location);
     console.log('user id:', user);
-    
-    const distanceInMeters = geolib.getDistance(user_location, target_location);
 
-    console.log('Distance between user and target(m):', distanceInMeters);
+    if (currentExperience !== 0) {
+      let experience = experiences.find(exp => exp.id === currentExperience);
+      const distanceInMeters = geolib.getDistance(user_location, experience.location);
 
-    // Broadcast location to all other clients
-    socket.broadcast.emit('user-location', user_location, user);
+      console.log('Distance between user and target(m):', distanceInMeters);
+
+      // Broadcast location to all other clients
+      if (distanceInMeters < experience.minDistance)
+        experience.nUsersIn += 1;
+    }
   });
+
+  socket.broadcast.emit('exp-location', experiences);
 
   socket.on('disconnect', () => {
     console.log('A user disconnected');
