36826c6bd02e8950a6c0a7da3a862b9ca6ff2761
SadLunch <olaadeus12345z@gmail.com>
2024-10-18 14:34:26 +0100

    test to send the experiences

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const geolib = require('geolib');
 
-
+let experiences = [
+  {id: 1, location: {lat: 38.710712892523695, lng: -9.14116628132217}, minDistance: 10, name: 'Bertrand', image: '', nUsersIn: 0},
+  {id: 2, location: {lat: 38.709899442837504, lng: -9.141786418367884}, minDistance: 10, name: 'Modelo', image: '', nUsersIn: 0},
+  {id: 3, location: {lat: 38.71051049617013, lng: -9.142228602930569}, minDistance: 10, name: 'Estatua', image: '', nUsersIn: 0},
+  {id: 4, location: {lat: 38.709363862713104, lng: -9.141031676168348}, minDistance: 10, name: 'MUP', image: '', nUsersIn: 0},
+  {id: 5, location: {lat: 38.65109183413688, lng: -9.173760194424412}, minDistance: 20, name: 'Casa Teste', image: '', nUsersIn: 0}
+]
 
 const app = express();
 app.use(cors());
@@ -31,7 +37,7 @@ io.on('connection', (socket) => {
     console.log('Distance between user and target(m):', distanceInMeters);
 
     // Broadcast location to all other clients
-    socket.broadcast.emit('user-location', user_location, user);
+    socket.broadcast.emit('user-location', user_location, user, experiences);
   });
 
   socket.on('disconnect', () => {
